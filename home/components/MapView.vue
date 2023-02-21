169f57a2dd7e20fa217f8ee6ecf9a8268e9e54be
Niklaus Maurer <niklaus.maurer@rethinc.ch>
2023-02-21 16:38:12 +0100

    Zooms out map

--- home/components/MapView.vue
+++ home/components/MapView.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d14899.645923046313!2d0.10710469276852869!3d38.68301189898952!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1sen!2sch!4v1676993083498!5m2!1sen!2sch" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+  <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d5456735.164942447!2d-2.0653050830785116!3d39.10348986206455!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x129e07218ee6ca7f%3A0xe28f318a19c07478!2sCarrer%20la%20Perdiu%2C%2039%2C%2003720%20Benissa%2C%20Alacant%2C%20Spain!5e1!3m2!1sen!2sch!4v1676993738129!5m2!1sen!2sch" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 </template>
 
 <style scoped lang="scss"></style>
