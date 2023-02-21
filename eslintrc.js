51bf32f187ed55ad411739676f5acf551870a5f2
Niklaus Maurer <niklaus.maurer@rethinc.ch>
2023-02-21 14:34:09 +0100

    Adds verify step

--- eslintrc.js
+++ eslintrc.js
@@ -1,12 +1,12 @@
 module.exports = {
-    parser: 'vue-eslint-parser',
-    parserOptions: {
-        ecmaVersion: 2020,
-        parser: {
-            ts: "@typescript-eslint/parser",
-        }
-    },
-    extends: ['plugin:vue/vue3-recommended'],
-    ignorePatterns: ['dist/', 'node_modules/', '*.json', '.temp/'],
-    rules: { /**/ },
-};
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    parser: {
+      ts: '@typescript-eslint/parser'
+    }
+  },
+  extends: ['plugin:vue/vue3-recommended'],
+  ignorePatterns: ['dist/', 'node_modules/', '*.json', '.temp/'],
+  rules: { /**/ }
+}
