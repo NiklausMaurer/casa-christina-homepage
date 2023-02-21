f3d3f9dc9f0e30019213bbc1418847a9d88e9a40
Niklaus Maurer <niklaus.maurer@rethinc.ch>
2023-02-21 16:30:14 +0100

    Turns off appearance

--- home/.vitepress/config.ts
+++ home/.vitepress/config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
     lang: 'de-CH',
     title: 'Casa Christina',
     description: 'Ferien in Moraira, Spanien',
+    appearance: false,
     themeConfig: {
         nav: [
             { text: 'Anfahrt', link: '/arrival' },
