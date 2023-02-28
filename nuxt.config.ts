2a895b3c059b0ec5918ab9c454d19d2f69f0cd7b
Niklaus Maurer <niklaus.maurer@rethinc.ch>
2023-02-28 16:04:10 +0100

    Adds favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,33 @@
 import { defineNuxtConfig } from 'nuxt/config'
 export default defineNuxtConfig({
   css: ['~/assets/styles/main.scss'],
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'apple-touch-icon',
+          sizes: '180x180',
+          href: '/apple-touch-icon.png',
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '32x32',
+          href: '/favicon-32x32.png',
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '16x16',
+          href: '/favicon-16x16.png',
+        },
+        { rel: 'manifest', href: '/site.webmanifest' },
+        { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#fcf9f5' },
+      ],
+      meta: [
+        { name: 'msapplication-TileColor', content: '#fcf9f5' },
+        { name: 'theme-color', content: '#fcf9f5' },
+      ],
+    },
+  },
 })
