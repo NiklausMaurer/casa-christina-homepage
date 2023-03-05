acf13f14e36250b89ffe07b7e831cbf4b2a57692
Niklaus Maurer <niklaus.maurer@rethinc.ch>
2023-03-05 18:00:41 +0100

    Adds more months to booking

--- pages/booking.vue
+++ pages/booking.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts"></script>
 
 <template>
-  <booking-calendar year="2023" month="3" />
+  <div class="container">
+    <booking-calendar year="2023" month="3" />
+    <booking-calendar year="2023" month="4" />
+    <booking-calendar year="2023" month="5" />
+  </div>
 </template>
 
 <style scoped lang="scss"></style>
