e9f24ede517de77a62fa3a5c38ed8259841af94e
Niklaus Maurer <niklaus.maurer@rethinc.ch>
2023-03-06 22:19:26 +0100

    adds Reservation hasOverlapWith(...)

--- composables/Reservation.ts
+++ composables/Reservation.ts
@@ -1,8 +1,21 @@
+import { isSameDayOrBefore } from './Date'
+
 export enum AvailabilityState {
   Free,
+  Unavailable,
 }
 
 export interface Reservation {
   startDate: Date
   endDate: Date
+}
+
+export const hasOverlapWith = (
+  reservation: Reservation,
+  date: Date
+): boolean => {
+  return (
+    isSameDayOrBefore(reservation.startDate, date) &&
+    isSameDayOrBefore(date, reservation.endDate)
+  )
 }
